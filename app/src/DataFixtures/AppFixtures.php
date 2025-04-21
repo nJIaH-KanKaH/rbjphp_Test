3e72a3d085df6a05c4ce10d6a68a77a8249c7060
nJIaH-KanKaH <53330165+nJIaH-KanKaH@users.noreply.github.com>
2025-04-21 09:14:37 +0300

    Main (#1)
    * Updates on existing files:
    1.Added a new entity Category with its repository, form, and controller.
    2.Updated the Book entity to include a relationship with Category.
    3.Created a migration for the new Category entity.
    4.Updated the fixtures to include sample categories on app load.
    5.Added html templates for CRUD operations on Category.
    6.Updated the base template to include links to the new Category pages.
    7.Updated the Book templates to include edit for Category.
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Book;
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Faker\Factory;
 use Doctrine\Persistence\ObjectManager;
@@ -12,12 +13,29 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
+        $bookCategories = [
+            'Fiction',
+            'Non-Fiction',
+            'Mystery',
+            'Thriller',
+            'Romance',
+            'Science Fiction',
+            'Fantasy',
+            'Horror',
+            'Historical Fiction',
+            'Biography',
+            'History',
+        ];
+        $categories = [];
+        foreach ($bookCategories as $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $categories[] = $category;
+            $manager->persist($category);
+        }
 
         for ($i = 0; $i < 100; $i++) {
             $description = $faker->paragraph;
-            if (strlen($description) > 80) {
-                $description = substr($description, 0, 80) ;
-            }
 
             $book = new Book();
             $book
@@ -26,6 +44,7 @@ class AppFixtures extends Fixture
                 ->setPublishedAt($faker->dateTimeBetween('-30 years', 'now'))
                 ->setDescription($description)
                 ->setIsbn($faker->isbn13)
+                ->setCategory($faker->randomElement($categories))
             ;
             $manager->persist($book);
         }
